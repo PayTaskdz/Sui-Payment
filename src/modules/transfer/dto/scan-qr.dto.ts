634466f5b57691f9bde9e55f719c828b5fd96746
Woft257 <h.hoangtuan257@gmail.com>
2026-01-18 11:07:25 +0700

    update scan qr

--- src/modules/transfer/dto/scan-qr.dto.ts
+++ src/modules/transfer/dto/scan-qr.dto.ts
@@ -3,7 +3,4 @@ import { IsString } from 'class-validator';
 export class ScanQrDto {
   @IsString()
   qrString: string;
-
-  @IsString()
-  userId: string; // Current user scanning (for audit/future features)
 }
